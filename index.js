ff28a10c1e3d101cb36e3468f34c600868f6a255
Erick <edeleon-2019598@kinal.edu.gt>
2021-07-08 08:27:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 var mongoose = require('mongoose');
 var app = require('./app')
+var port= process.env.PORT|| 3000;
 var inicioUsuario = require('./controllers/user.controller');
 
 mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb+srv://admin:admin@cluster0.foqzx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useFindAndModify: true})
     .then(()=>{
         console.log('conectado a la bd');
         inicioUsuario.createInit();
