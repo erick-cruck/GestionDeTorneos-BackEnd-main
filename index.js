aa0fe0a5a610e87b76d7e0d7a6c67f81944a7fc7
Erick <edeleon-2019598@kinal.edu.gt>
2021-07-08 08:25:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 
 var mongoose = require('mongoose');
 var app = require('./app')
-var port= process.emv.PORT|| 3000;
 var inicioUsuario = require('./controllers/user.controller');
 
 mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost:27017/gestionDeTorneosBD', {useNewUrlParser: true, useFindAndModify: true})
     .then(()=>{
         console.log('conectado a la bd');
         inicioUsuario.createInit();
